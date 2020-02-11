4b55535d8e23efdae86db78751c3e292832545b7
bimal kaluarachchi <bimal.kaluarachchi@bidenergy.com>
2020-02-11 22:19:31 +1100

    hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App=() =>{
+
+  const [count,setCount] =useState(0);
+
+  const incrementCount=()=>{
+    setCount(count+1)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <button onClick={incrementCount}><h1>I was clicked {count} times.</h1></button>
   );
-}
+};
 
 export default App;
